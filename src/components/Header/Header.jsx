8b346e2a996b1b4f05682703f5a8d6e3d916eaf7
Douglas <douglasdanielx@gmail.com>
2025-06-21 09:29:10 -0300

    style: adiciona css ao header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,10 @@
 'use client'
-
+import style from "./Header.module.css"
 import Image from "next/image";
 import imgLogo from "../../../public/barbearia.png"
 import { useState } from "react";
 import { GrSolaris } from "react-icons/gr";
-import { BsMoonStars } from "react-icons/bs";
+import { FiMoon } from "react-icons/fi";
 
 export default function Header(){
     const [temaEscuro, setTemaEscuro] = useState(false)
@@ -12,12 +12,13 @@ export default function Header(){
 function TrocarTema(){
     setTemaEscuro(!temaEscuro)
 }
+
     return (
-        <header>
-            <div>
-                <Image src={imgLogo}></Image>
-                <button onClick={TrocarTema}>
-                    {temaEscuro? <GrSolaris/>:<BsMoonStars/>}
+        <header className={`${temaEscuro ? style.tema_escuro : ''}`}>
+            <div className={style.header_box}>
+                <Image src={imgLogo} className={style.image_logo } alt="logo da barbearia, rosto com uma lamina em casa lado"></Image>
+                <button onClick={TrocarTema} className={style.button_tema}>
+                    {temaEscuro? <FiMoon className={style.icon} fill="white"/>:<GrSolaris className={style.icon}/>}
                 </button>
             </div>
         </header>
